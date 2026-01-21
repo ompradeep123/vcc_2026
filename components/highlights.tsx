e24f23c71825c68a3d68dd8330f91915f3f5509b
Ompradeep <ompradeep2004@gmail.com>
2026-01-21 14:29:18 +0530

    commit

--- components/highlights.tsx
+++ components/highlights.tsx
@@ -38,8 +38,8 @@ const events = [
 
 export default function Highlights() {
   return (
-    <section id="highlights" className="py-12 sm:py-16 md:py-20 px-4 sm:px-6 lg:px-8 relative\">
-      <div className=\"max-w-7xl mx-auto\">
+    <section id="highlights" className="py-12 sm:py-16 md:py-20 px-4 sm:px-6 lg:px-8 relative">
+      <div className="max-w-7xl mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
